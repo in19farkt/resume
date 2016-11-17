00012ffc3ffbc007de9ed1a7fb1c69c133689fde
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-11-17 12:58:32 +0700

    Add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const webpack = require('webpack');
@@ -6,62 +5,62 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    context: __dirname + '/frontend',
+  context: `${__dirname}/frontend`,
 
-    entry: {
-        main: './index.js'
-    },
+  entry: {
+    main: './index.js',
+  },
 
-    output: {
-        path: __dirname + '/public',
-        filename: "index.js"
-    },
+  output: {
+    path: `${__dirname}/public`,
+    filename: 'index.js',
+  },
 
-    devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : null,
+  devtool: NODE_ENV === 'development' ? 'cheap-inline-module-source-map' : null,
 
-    plugins: [
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        }),
-        new ExtractTextPlugin('index.css', {allChunks: true}),
-        new HtmlWebpackPlugin({filename: 'index.html', template: './index.pug'})
-    ],
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV),
+    }),
+    new ExtractTextPlugin('index.css', { allChunks: true }),
+    new HtmlWebpackPlugin({ filename: 'index.html', template: './index.pug' }),
+  ],
 
-    resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js', '.styl', '.pug']
-    },
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js', '.styl', '.pug'],
+  },
 
-    resolveLoader: {
-        modulesDirectories: ['node_modules'],
-        modulesTemplates: ['*-loader', '*'],
-        extensions: ['', '.js']
-    },
+  resolveLoader: {
+    modulesDirectories: ['node_modules'],
+    modulesTemplates: ['*-loader', '*'],
+    extensions: ['', '.js'],
+  },
 
-    module: {
+  module: {
 
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel',
-            exclude: /node_modules/
-        }, {
-            test: /\.pug$/,
-            loader: 'pug'
-        }, {
-            test: /\.css$/,
-            loader: 'style!css' //!autoprefixer?browsers=last 2 versions'
-        }, {
-            test: /\.styl$/,
-            loader: ExtractTextPlugin.extract('!css!stylus?resolve url') //!autoprefixer?browsers=last 2 versions'
-        }, {
-            test: /\.(png|ipg|svg|ttf|eot|woff|woff2)$/,
-            loader: 'file?name=[path][name].[ext]'
-        }]
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+    }, {
+      test: /\.pug$/,
+      loader: 'pug',
+    }, {
+      test: /\.css$/,
+      loader: 'style!css', // !autoprefixer?browsers=last 2 versions'
+    }, {
+      test: /\.styl$/,
+      loader: ExtractTextPlugin.extract('!css!stylus?resolve url'), // !autoprefixer?browsers=last 2 versions'
+    }, {
+      test: /\.(png|ipg|svg|ttf|eot|woff|woff2)$/,
+      loader: 'file?name=[path][name].[ext]',
+    }],
 
-    },
+  },
 
-    devServer: {
-        contentBase: './public'
-    }
+  devServer: {
+    contentBase: './public',
+  },
 };
